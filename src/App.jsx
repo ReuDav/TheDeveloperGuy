2910d356dad4057ca61f1dee73a9fc725e8bd8f4
Reucov David <reucovd@gmail.com>
2024-01-26 09:27:47 +0100

    Added Call to action section
    This is the part of the website that most users will notice at first glance. With a bit of catchy text with a call to action button, and with a relevant image.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './index.css'
 import NavigationBar from './assets/Components/NavigationBar.jsx'
-
+import CallToAction from "./assets/Components/CallToAction.jsx";
 function App() {
   return (
     <div>
-      <NavigationBar></NavigationBar>
+        <NavigationBar></NavigationBar>
+        <CallToAction></CallToAction>
     </div>
   )
 }
